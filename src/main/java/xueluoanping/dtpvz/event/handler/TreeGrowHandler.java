c2b44d6cd97dcc920387400560db5d9d201339a8
joe-vetterk <my1233215@126.com>
2023-11-24 11:52:23 +0800

    update config
    1.When a sapling becomes a sapling, it has a chance to grow original essence ore.
    
    2.Updated the data configuration so that nut trees will now drop nuts when they fall down.

--- src/main/java/xueluoanping/dtpvz/event/handler/TreeGrowHandler.java
+++ src/main/java/xueluoanping/dtpvz/event/handler/TreeGrowHandler.java
@@ -1,7 +1,12 @@
 package xueluoanping.dtpvz.event.handler;
 
+import com.ferreusveritas.dynamictrees.api.event.TransitionSaplingToTreeEvent;
 import com.ferreusveritas.dynamictrees.event.SpeciesPostGenerationEvent;
+import com.hungteen.pvz.PVZConfig;
 import com.hungteen.pvz.common.block.BlockRegister;
+import com.hungteen.pvz.utils.MathUtil;
+import net.minecraft.block.Block;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -12,11 +17,31 @@ public class TreeGrowHandler {
     public static final TreeGrowHandler instance = new TreeGrowHandler();
     @SubscribeEvent
     public void onTreeGrowEvent(SpeciesPostGenerationEvent event) {
-        Random random =event.getWorld().getRandom();
-        if (random.nextInt(16)==0)
+        // Random random =event.getWorld().getRandom();
+        final double chance = PVZConfig.COMMON_CONFIG.BlockSettings.SaplingTurnChance.get();
+        if(MathUtil.randDouble(event.getWorld().getRandom(), chance) && event.getRootPos().getY() > 2)
         {
 //            LOGGER.info(""+event);
             event.getWorld().setBlock(event.getRootPos(),BlockRegister.ORIGIN_ORE.get().defaultBlockState(),0);
+        }
+    }
+
+    @SubscribeEvent
+    public void onTreeGrowEvent(TransitionSaplingToTreeEvent event) {
+        Random random =event.getWorld().getRandom();
+        // LOGGER.info(""+event+event.getPos()+event.getSpecies());
+        // if (random.nextInt(16)==0)
+        // {
+        //     event.getWorld().setBlock(event.getPos().below(),BlockRegister.ORIGIN_ORE.get().defaultBlockState(), Constants.BlockFlags.BLOCK_UPDATE);
+        // }
+        if(! event.getWorld().isClientSide()) {
+            final boolean isBlocked = ! event.getWorld().isEmptyBlock(event.getPos().above());
+            if(PVZConfig.COMMON_CONFIG.RuleSettings.AllowNaturalTurnOrigin.get() || isBlocked) {
+                final double chance = PVZConfig.COMMON_CONFIG.BlockSettings.SaplingTurnChance.get();
+                if(MathUtil.randDouble(event.getWorld().getRandom(), isBlocked ? chance * 1.2 : chance) && event.getPos().getY() > 2) {
+                    event.getWorld().setBlock(event.getPos().below(),BlockRegister.ORIGIN_ORE.get().defaultBlockState(), Constants.BlockFlags.BLOCK_UPDATE);
+                }
+            }
         }
     }
 }
